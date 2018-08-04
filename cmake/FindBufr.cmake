30934a23a33344e374c9c008473a9ec8d73aa6ae
Travis Sluka <travis.sluka@noaa.gov>
2018-08-04 15:42:16 -0400

    test obsqc.nml file added

--- cmake/FindBufr.cmake
+++ cmake/FindBufr.cmake
@@ -1,4 +1,4 @@
-find_library(BUFR_LIBRARY NAMES bufr)
+find_library(BUFR_LIBRARY NAMES bufr BUFR bufrlib)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Bufr DEFAULT_MSG 
   BUFR_LIBRARY)
